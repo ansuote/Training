69fe50aeedb42fada799439de83f627c443518c0
ansuote <ansuote2011@gmail.com>
2019-03-22 11:59:21 +0800

    修复tinker在5.0以下机器上运行分dex报错的问题

--- app/src/main/java/com/lkl/ansuote/traning/module/share/ShareActivity.kt
+++ app/src/main/java/com/lkl/ansuote/traning/module/share/ShareActivity.kt
@@ -1,5 +1,6 @@
 package com.lkl.ansuote.traning.module.share
 
+import android.content.ComponentName
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -21,24 +22,39 @@ class ShareActivity : AppCompatActivity(), View.OnClickListener{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.share_activity)
         btn_picture_share.setOnClickListener(this)
+        btn_wechat_share?.setOnClickListener(this)
     }
 
 
     override fun onClick(view: View?) {
+        val imagePath = "/storage/emulated/0/Download/ic_launcher.png"
+        //由文件得到uri
+        val imageUri =  Uri.fromFile(File(imagePath))
         when(view) {
             btn_picture_share -> {
                 //测试写死地址
-                val imagePath = "/storage/emulated/0/Download/001.png"
-                //由文件得到uri
-                val imageUri = Uri.fromFile(File(imagePath))
-
                 val shareIntent = Intent()
                 shareIntent.action = Intent.ACTION_SEND
                 shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri)
                 shareIntent.type = "image/*"
                 startActivity(Intent.createChooser(shareIntent, "分享到"))
             }
+            btn_wechat_share -> {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.setType("image/*");
+                //发送图片到朋友圈
+                /*val comp = ComponentName("com.tencent.mm",
+                        "com.tencent.mm.ui.tools.ShareToTimeLineUI")*/
+                //发送图片给好友。
+                val comp = ComponentName("com.tencent.mm", "com.tencent.mm.ui.tools.ShareImgUI")
+                intent.setComponent(comp)
+                //intent.setAction("android.intent.action.SEND_MULTIPLE") //该属性会导致分享失败
+                intent.setType("image/*")
+                //这个就是标题了
+                intent.putExtra("Kdescription" + System.currentTimeMillis(), "多图分享的标题")
+                intent.putExtra(Intent.EXTRA_STREAM, imageUri)
+                startActivity(intent)
+            }
         }
     }
-
 }
